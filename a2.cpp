65b2920e4ca1ca5b1cb388d7905e03d61b4d9118
Lena Huszar <lena@huszar.de>
2021-01-07 16:50:06 +0100

    kurze Nachricht

--- a2.cpp
+++ a2.cpp
@@ -5,6 +5,10 @@
 using namespace ::std::literals;
 
 int main() {
-	std::cout << "Hallo, Welt" << "\n";
+	std::cout << "Hallo, wunderbare Welt" << "\n";
 	std::cout << "hm, das ist ja spannend" << std::endl;
+	std::cout << "irgendeine faszinierende Nachricht" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5 Zwerge" << std::endl;
+	std::cout << "6" << std::endl;
 }
