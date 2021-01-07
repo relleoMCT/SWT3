b898ef4d9f9e5ec7f3ec58b82c903741379f4e55
relleoMCT <77016124+relleoMCT@users.noreply.github.com>
2021-01-07 16:36:32 +0100

    neu

--- a2.cpp
+++ a2.cpp
@@ -7,4 +7,8 @@ using namespace ::std::literals;
 int main() {
 	std::cout << "Hallo, Welt" << "\n";
 	std::cout << "hm, das ist ja spannend" << std::endl;
+	std::cout << "irgendeine Nachricht" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "6" << std::endl;
 }
